dda30208bb98d07f3e3ffdfbec7bb3a4a0d193ed
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-17 13:32:58 +0500

    PasswordGenerator - Functionality added for generating password

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactSwitch from "react-switch";
 
 const PasswordGenerator = () => {
+  const [password, setPassword] = useState("");
+  const [passwordLength, setPasswordLength] = useState(8);
+  const [includeUpperCase, setIncludeUpperCase] = useState(true);
+  const [includeNumbers, setIncludeNumbers] = useState(true);
+  const [includeSpecialCharacter, setIncludeSpecialCharacter] = useState(true);
+
+  const generatePassword = () => {
+    const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "1234567890";
+    const specialCharacters = "!@#$%^&*()-_{}'?/>.<,;`~|[]₱$﷼";
+
+    let validChars = lowerCaseChars;
+
+    if (includeUpperCase) {
+      validChars += upperCaseChars;
+    }
+
+    if (includeNumbers) {
+      validChars += numbers;
+    }
+
+    if (includeSpecialCharacter) {
+      validChars += specialCharacters;
+    }
+
+    let generatePassword = "";
+
+    for (let i = 0; i < passwordLength; i++) {
+      const randomIndex = Math.floor(Math.random() * validChars.length);
+      generatePassword += validChars.charAt(randomIndex);
+    }
+    setPassword(generatePassword);
+  };
+
   return (
     <div className="pt-8 text-white font-bold">
       <div className="flex flex-col items-center">
@@ -9,26 +44,56 @@ const PasswordGenerator = () => {
           <div className="mb-4 flex items-center justify-between">
             <label htmlFor="passwordLength">Password Length: </label>
             <input
+              value={passwordLength}
+              onChange={(e) => setPasswordLength(e.target.value)}
               className="w-12 bg-purple-700 pl-2"
               type="number"
               name="passwordLength"
               id="passwordLength"
+              min="4"
+              max="64"
             />
           </div>
           <div className="mb-4 flex items-center justify-between">
             <label>Include UpperCase: </label>
-            <ReactSwitch height={20} width={40} handleDiameter={18} />
+            <ReactSwitch
+              checked={includeUpperCase}
+              onChange={() => setIncludeUpperCase(!includeUpperCase)}
+              height={20}
+              width={40}
+              handleDiameter={18}
+            />
           </div>
           <div className="mb-4 flex items-center justify-between">
             <label>Include Numbers: </label>
-            <ReactSwitch height={20} width={40} handleDiameter={18} />
+            <ReactSwitch
+              checked={includeNumbers}
+              onChange={() => setIncludeNumbers(!includeNumbers)}
+              height={20}
+              width={40}
+              handleDiameter={18}
+            />
           </div>
           <div className="mb-4 flex items-center justify-between">
             <label>Include Special Characters: </label>
-            <ReactSwitch height={20} width={40} handleDiameter={18} />
+            <ReactSwitch
+              checked={includeSpecialCharacter}
+              onChange={() =>
+                setIncludeSpecialCharacter(!includeSpecialCharacter)
+              }
+              height={20}
+              width={40}
+              handleDiameter={18}
+            />
           </div>
-          <button className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white font-bold rounded-md p-3">Generate Password</button>
+          <button
+            className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white font-bold rounded-md p-3"
+            onClick={generatePassword}
+          >
+            Generate Password
+          </button>
         </div>
+        <h2>{password}</h2>
       </div>
     </div>
   );
