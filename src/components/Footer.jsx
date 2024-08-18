db8b6784f93d8995ef1210404116b5b3e55442d7
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-18 07:32:02 +0500

    Footer - Footer added successfully

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="mt-12 p-4 bg-gradient-to-r from-green-400 to-blue-500 text-white text-center font-bold rounded-t-md">
+      <p>Developed by Syed Muhammad Hussnain Raza</p>
+    </footer>
+  );
+};
+
+export default Footer;
