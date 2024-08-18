85bbac183a7c8edaae5362c4e8e161519a28dc4d
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-18 07:33:25 +0500

    Footer - Footer added successfully in App Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import PasswordGenerator from "./components/PasswordGenerator";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App h-screen bg-gradient-to-r from-red-200 via-pink-200">
-      <h1 className="text-3xl font-bold pt-16 text-blue-900">Password Generator</h1>
+      <h1 className="text-3xl font-bold pt-16 text-blue-900">
+        Password Generator
+      </h1>
       <PasswordGenerator></PasswordGenerator>
+      <Footer />
     </div>
   );
 }
